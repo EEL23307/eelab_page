2020956b60deb509398f0837689e58ebbcc8512f
EEL_SKKU <eelab23307@gmail.com>
2026-01-26 20:53:19 +0900

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -35,7 +35,7 @@ const Navbar: React.FC<NavbarProps> = ({ activeTab, setActiveTab }) => {
           {/* [수정 2] 로고 이미지로 교체된 부분 */}
           <div className="flex items-center cursor-pointer" onClick={() => handleNavClick('Home')}>
             <img 
-              src="images/logo_new.png" 
+              src="images/logo_green.png" 
               alt="Energy Engineering Lab Logo" 
               className="h-10 md:h-12 w-auto object-contain" 
             />
